8b1b22238010c1fc1fd64f1b2f81af8e28cb8c92
curtismoncoq <147111716+curtismoncoq@users.noreply.github.com>
2024-02-02 02:00:23 +0000

    make

--- philo_bonus/srcs/rw_data_2.c
+++ philo_bonus/srcs/rw_data_2.c
@@ -1,47 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rw_data.c                                          :+:      :+:    :+:   */
+/*   rw_data_2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cumoncoq <cumoncoq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:19:25 by cumoncoq          #+#    #+#             */
-/*   Updated: 2024/02/02 01:42:09 by cumoncoq         ###   ########.fr       */
+/*   Updated: 2024/02/02 01:41:53 by cumoncoq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long	r_ate(t_data *d)
+int	r_run(t_data *d)
 {
-	long long	res;
+	int	res;
 
-	sem_wait(d->mutex_eat);
-	res = d->ate;
-	sem_post(d->mutex_eat);
+	sem_wait(d->mutex_run);
+	res = d->running;
+	sem_post(d->mutex_run);
 	return (res);
 }
 
-void	w_ate(t_data *d, long long value)
+void	w_run(t_data *d)
 {
-	sem_wait(d->mutex_eat);
-	d->ate = value;
-	sem_post(d->mutex_eat);
+	sem_wait(d->mutex_run);
+	d->running = 0;
+	sem_post(d->mutex_run);
 }
 
-long long	r_meals(t_data *d)
+int	r_max(t_data *d)
 {
-	long long	res;
+	int	res;
 
-	sem_wait(d->mutex_meals);
-	res = d->meals;
-	sem_post(d->mutex_meals);
+	sem_wait(d->mutex_run);
+	res = d->arg->max_eat;
+	sem_post(d->mutex_run);
 	return (res);
 }
 
-void	w_meals(t_data *d)
+void	w_max(t_data *d, int value)
 {
-	sem_wait(d->mutex_meals);
-	d->meals++;
-	sem_post(d->mutex_meals);
+	sem_wait(d->mutex_run);
+	d->arg->max_eat = value;
+	sem_post(d->mutex_run);
 }
